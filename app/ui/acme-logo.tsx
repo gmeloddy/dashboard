560956ed541ee16bbe8fc337b328d00c88119db3
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-11-06 11:45:57 +0100

    first commit

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -8,7 +8,10 @@ export default function AcmeLogo() {
       className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
-      <Image src="/images/logo.png" alt="Acme Logo" className="text-[44px]" />
+      <Image src="/images/logo.png" 
+      width={20}
+      height={20}
+      alt="Acme Logo" className="text-[44px]" />
     </div>
   );
 }
