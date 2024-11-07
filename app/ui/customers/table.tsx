a43d070316867ad0bb00702b7183335c44f89c4b
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-11-07 09:21:44 +0100

    etst

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -2,14 +2,14 @@ import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
 import Search from '@/app/ui/search';
 import {
-  CustomersTableType,
-  FormattedCustomersTable,
+  // CustomersTableType,
+  // FormattedCustomersTable,
 } from '@/app/lib/definitions';
 
 export default async function CustomersTable({
   customers,
 }: {
-  customers: FormattedCustomersTable[];
+  customers: any[];
 }) {
   return (
     <div className="w-full">
