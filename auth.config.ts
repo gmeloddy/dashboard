000d7625266bdf809d7d1f582241ea52d464d19e
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-11-07 11:26:33 +0100

    etst

--- auth.config.ts
+++ auth.config.ts
@@ -12,10 +12,12 @@ export const authConfig = {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        return Response.redirect(new URL('https://dataversedashboard.vercel.app/dashboard', nextUrl));
       }
       return true;
     },
   },
   providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
+
+
