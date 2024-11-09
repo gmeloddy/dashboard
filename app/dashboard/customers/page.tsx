b87a3acbd34f5a750fe77fe311b9d8d24fe0b2fa
Olanrewaju <lawallanre.abbahouse@gmail.com>
2024-11-09 12:25:16 +0100

    Added toastify functionality

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CustomersPage = () => {
   return (
-    <div>CustomersPage</div>
+    <div>Run Ads</div>
   )
 }
 
--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const InvoicesPage = () => {
   return (
-    <div>InvoicesPage</div>
+    <div>Recommendation</div>
   )
 }
 
